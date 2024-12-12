5f67f304c1626fff7ff503e50671978b7c8f4cde
teame-sy <transwiki@proton.me>
2024-12-12 10:50:02 +0900

    임시로 일단 정수 입력과 오류 처리만 대충 개발update

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# all
+a = input("정수를 하나 입력해주세요 ")
+print("입력하신 정수는 {} 입니다. ".format(a))
+b = input("다른 정수를 하나만 더 입력해주세요")
+print("입력하신 정수는 {} 입니다. ".format(b))
+print("--------")
+try:
+    int(a)
+    int(b)
+
+    print("a+b= {}".format(a+b))
+
+
+except Valueerror as exception:
+    print("정수를 입력해주라고요.. 제발.. 다시 프로그램을 시작해주세요..")
+
+    
+
+
